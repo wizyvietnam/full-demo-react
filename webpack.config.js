9603c88c946dbc6b6fe5dd2d5621392c2abead34
Vi Hoang <dev.wizy@gmail.com>
2019-02-08 11:01:54 +0700

    Change directory

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:9000',
     'webpack/hot/only-dev-server',
-    './index.js'
+    './src/index.js'
   ],
   output: {
     path: path.join(__dirname, 'www'),
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'source-map',
   context: __dirname,
   entry: [
-    './index.js'
+    './src/index.js'
   ],
   output: {
     path: path.join(__dirname, 'www'),
